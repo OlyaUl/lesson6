9e19ac1ed3ed89e0e2518f3ad12640006d1ae39e
Olya <johndoe@example.com>
2017-09-21 16:09:58 +0300

    decorator  main.py

--- main.py
+++ main.py
@@ -2,13 +2,7 @@
 # codding = utf-8
 from abc import ABCMeta,abstractmethod
 import os
-
-
-#! env bin/python
-# codding = utf-8
-from abc import ABCMeta,abstractmethod
-import os
-
+from functools import total_ordering, lru_cache
 
 def add(p1, p2, take=False):
     """
@@ -21,6 +15,18 @@ def add(p1, p2, take=False):
     pass
 help(add)
 
+@lru_cache(3)
+def fib1(n):
+    print("enter {}".format(n))
+    if n < 2:
+        return n
+    return fib1(n-1) + fib1(n-2)
+
+#n1 = int(input())
+#print(fib1(n1))
+
+print("--------------")
+@total_ordering
 class Bot:
 
     def __init__(self, name):
@@ -35,17 +41,18 @@ class Bot:
     def __add__(self, other):
         return str(self) + other
 
+    #@total_ordering
     def __eq__(self, other):
         return self.name == other
 
+    #@total_ordering
     def __lt__(self, other):
         return len(self.name) < len(other.name)
 
     def __repr__(self):
         return self.name + str(id(self))
 
-    '''def say_hi(self):
-        print("My name is {self.name}".format(**locals()))'''
+
 
     def _say_stdout(self):
         print("My name is {self.name}".format(**locals()))
@@ -70,66 +77,51 @@ print(lstBot.sort(reverse=True))
 print(lstBot)
 
 print(myBot == myB)
-print(myBot < myB)
+print(myBot != myB)
+
+print(myBot > myB)
+print(myBot <= myB)
 
 
-myBot.say_hi()
-print(myBot.testttt)
+
+#myBot.say_hi()
+#print(myBot.testttt)
 #print(vars(myBot))
 #print(vars(Bot))
 #print(help(myBot))
 #print(dir(myBot))
-Bot("ddd").say_hi()
+#Bot("ddd").say_hi()
 # print(myBot)
+print("--------------")
 
 
-class AngryBot(Bot):
-    def say_hi(self):
-        print("angry test")
+def logged(func):
+    def wrapped(*args, **kwargs):
+        print('entered into:', func.__name__)
+        result = func(*args, **kwargs)
+        print('got result:', result)
+        return result
+    return wrapped
 
-a_bot = AngryBot("The bad")
-a_bot.say_hi()
+def output(message):
+    print(message)
 
+@logged
+def logged_output(message):
+    print(message)
 
-class EvilBot(Bot):
-
-    @staticmethod
-    def __new__(cls, *args, **kwargs):
-        return AngryBot(*args, **kwargs)
-
-
-evil = EvilBot("ha-ha")
-print(evil.say_hi())
-
-
-class Sayer:
-    def say(self, message):
-        pass
+output('hello')
+print('-' * 50)
+logged_output('hello')
 
 
-class HttpHandler(metaclass=ABCMeta):
-
-    @abstractmethod
-    def get(self, location):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def get(self, location, data):
-        raise NotImplementedError()
-
-
-class Memoize(HttpHandler):
-    value = "default"
-    def get(self, location):
-        return self.value
-
-    def post(self,location,data):
-        self.value = data
-
-
-class HomePage(HttpHandler, Sayer):
-    def say(self, message):
-        print('YOLO' + message)
-
-    def get(self, location):
-        return "HomePage"
+def maybe_logged(do_log=True):
+    def logged(func):
+        def wrapped(*args, **kwargs):
+            if do_log:
+                print('entered into:', func.__name__)
+            result = func(*args, **kwargs)
+            if do_log:
+                print('got result:', result)
+            return result
+        return wrapped
